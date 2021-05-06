3c04b7d3e0e9f26017301bbf1ba6f0c41c392056
naka.haru <8runaka6@gmail.com>
2021-05-06 22:49:48 +0900

    Add User update

---
+++ src/controllers/users/UserUpdateServlet.java
@@ -0,0 +1,82 @@
+package controllers.users;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import models.User;
+import models.validators.UserValidator;
+import utils.DBUtil;
+import utils.EncryptUtil;
+
+/**
+ * Servlet implementation class UserUpdateServlet
+ */
+@WebServlet("/users/update")
+public class UserUpdateServlet extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
+    public UserUpdateServlet() {
+        super();
+    }
+
+    /**
+     * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+     */
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String _token = request.getParameter("_token");
+        if(_token != null && _token.equals(request.getSession().getId())){
+            EntityManager em = DBUtil.createEntityManager();
+
+            User u = em.find(User.class, (Integer)(request.getSession().getAttribute("user_id")));
+
+            Boolean login_idDuplicateCheckFlag = false;
+            //現在と異なるパスワードが入力されていたら
+            //パスワードの入力値チェックを行う指定をする
+            Boolean passwordCheckFlag = true;
+            String password = request.getParameter("password");
+            if(password == null || password.equals("")){
+                passwordCheckFlag = false;
+            }else{
+                u.setPassword(
+                        EncryptUtil.getPasswordEncrypt(
+                                password,
+                                (String)this.getServletContext().getAttribute("pepper")
+                                )
+                        );
+            }
+
+            List<String> errors = UserValidator.validate(u, login_idDuplicateCheckFlag, passwordCheckFlag);
+            if(errors.size() > 0){
+                em.close();
+
+                request.setAttribute("_token", request.getSession().getId());
+                request.setAttribute("user", u);
+                request.setAttribute("errors", errors);
+
+                RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/users/edit.jsp");
+                rd.forward(request, response);
+            }else{
+                em.getTransaction().begin();
+                em.getTransaction().commit();
+                em.close();
+                request.getSession().setAttribute("flush", "更新が完了しました。");
+
+                request.getSession().removeAttribute("user_id");
+
+                response.sendRedirect(request.getContextPath() + "/users/index");
+            }
+        }
+    }
+
+}
