bd4cdc402015afced01f76ed6789b6250f5be02e
naka.haru <8runaka6@gmail.com>
2021-05-08 01:30:51 +0900

    Add Mypage Updeta

--- src/controllers/mypage/MypageUpdateServlet.java
+++ src/controllers/mypage/MypageUpdateServlet.java
@@ -1,4 +1,4 @@
-package controllers.users;
+package controllers.mypage;
 
 import java.io.IOException;
 import java.util.List;
@@ -14,18 +14,19 @@ import javax.servlet.http.HttpServletResponse;
 import models.User;
 import models.validators.UserValidator;
 import utils.DBUtil;
+import utils.EncryptUtil;
 
 /**
- * Servlet implementation class UserUpdateServlet
+ * Servlet implementation class MypageUpdateServlet
  */
-@WebServlet("/users/update")
-public class UserUpdateServlet extends HttpServlet {
+@WebServlet("/mypage/update")
+public class MypageUpdateServlet extends HttpServlet {
     private static final long serialVersionUID = 1L;
 
     /**
      * @see HttpServlet#HttpServlet()
      */
-    public UserUpdateServlet() {
+    public MypageUpdateServlet() {
         super();
     }
 
@@ -40,12 +41,20 @@ public class UserUpdateServlet extends HttpServlet {
             User u = em.find(User.class, (Integer)(request.getSession().getAttribute("user_id")));
 
             Boolean login_idDuplicateCheckFlag = false;
+            //現在と異なるパスワードが入力されていたら
+            //パスワードの入力値チェックを行う指定をする
             Boolean passwordCheckFlag = true;
-
-            u.setName(request.getParameter("name"));
-            u.setAdmin_flag(Integer.parseInt(request.getParameter("admin_flag")));
-            u.setDelete_flag(0);
-
+            String password = request.getParameter("password");
+            if(password == null || password.equals("")){
+                passwordCheckFlag = false;
+            }else{
+                u.setPassword(
+                        EncryptUtil.getPasswordEncrypt(
+                                password,
+                                (String)this.getServletContext().getAttribute("pepper")
+                                )
+                        );
+            }
             List<String> errors = UserValidator.validate(u, login_idDuplicateCheckFlag, passwordCheckFlag);
             if(errors.size() > 0){
                 em.close();
@@ -54,7 +63,7 @@ public class UserUpdateServlet extends HttpServlet {
                 request.setAttribute("user", u);
                 request.setAttribute("errors", errors);
 
-                RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/users/edit.jsp");
+                RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/mypages/edit.jsp");
                 rd.forward(request, response);
             }else{
                 em.getTransaction().begin();
@@ -64,7 +73,7 @@ public class UserUpdateServlet extends HttpServlet {
 
                 request.getSession().removeAttribute("user_id");
 
-                response.sendRedirect(request.getContextPath() + "/users/index");
+                response.sendRedirect(request.getContextPath() + "/index.html");
             }
         }
     }
